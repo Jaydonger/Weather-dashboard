8685f01d1c698fa1f88800003c9508bbac2de2ce
Jordan Pace <jordanpace95@gmail.com>
2023-01-23 14:43:24 -0700

    added starter code and files

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+
+
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+//var geoLocation = http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid={API key}
+
