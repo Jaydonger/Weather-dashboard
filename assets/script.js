6cdc51395b5996e748539013e34ae9883ba97373
Jordan Pace <jordanpace95@gmail.com>
2023-01-25 08:19:18 -0700

    adds button functionality and some variable decs to js file

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
-//var searchBtn = $('search');
+var cityState = document.querySelector('input');
+var searchBtn = document.querySelector('.btn');
 
+console.log(searchBtn);
+console.log(cityState);
 
-
-//function getWeather() {
-
-//}
 
 
 
@@ -15,4 +14,16 @@
 //var geoLocation = http://api.openweathermap.org/geo/1.0/zip?zip=E14,GB&appid={API key}
 
 
-//searchBtn.addEventListener('click', getWeather)
+searchBtn.addEventListener('click', function() {
+    var cityStateEl = cityState.value.split(', ');
+
+
+
+
+
+
+    console.log(cityStateEl);
+    console.log(cityState.value);
+    console.log(cityStateEl[0]);
+    console.log(cityStateEl[1]);
+});
